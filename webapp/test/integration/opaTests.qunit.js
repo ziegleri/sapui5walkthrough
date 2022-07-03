c72df22d08e69a630437c9ef98f6da96d94f99be
xras-386 <67341079+xras-386@users.noreply.github.com>
2022-07-03 18:46:07 +0200

    Initial

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sap/ui5/walkthrough/sapui5walkthrough/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapui5walkthrough/sapui5walkthrough/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
