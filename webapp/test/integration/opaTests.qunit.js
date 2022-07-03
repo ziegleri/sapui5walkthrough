f15bb77dae48aac46a8953655efe6b5fd384fd7d
xras-386 <67341079+xras-386@users.noreply.github.com>
2022-07-03 19:07:15 +0200

    git commit -m "Step 1"

--- webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["sap/ui5/walkthrough/sapui5walkthrough/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"sapui5walkthrough/sapui5walkthrough/test/unit/controller/Main.controller"
-], function () {
-	"use strict";
-});
