d90d0d76db109b39ba9d2ec5192345d2d347368b
xras-386 <67341079+xras-386@users.noreply.github.com>
2022-07-04 20:17:32 +0200

    git commit -m "Step7: JSON Model"

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,20 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast'
-], function(Controller, MessageToast) {
+    'sap/m/MessageToast',
+    "sap/ui/model/json/JSONModel"
+], function(Controller, MessageToast, JSONModel) {
     'use strict';
     return Controller.extend("mysap.ui5.walkthrough.controller.App", {
+        onInit: function(){
+            var oData = {
+                recepient: {
+                    name: "World"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+
         onShowHello: function(){
             MessageToast.show("Hello world!!!");
         }
