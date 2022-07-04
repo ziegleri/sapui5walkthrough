5a3c121b496f63f1bc35b54e5df637ae87c40105
xras-386 <67341079+xras-386@users.noreply.github.com>
2022-07-04 21:10:06 +0200

    git commit -m "Step 9: Component Configuration"

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,26 +1,9 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function(Controller, MessageToast, JSONModel, ResourceModel) {
+], function(Controller, MessageToast) {
     'use strict';
     return Controller.extend("mysap.ui5.walkthrough.controller.App", {
-        onInit: function(){
-            var oData = {
-                recipient: {
-                    name: "World"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-         // set i18n model on view
-         var i18nModel = new ResourceModel({
-            bundleName: "mysap.ui5.walkthrough.i18n.i18n"
-         });
-         this.getView().setModel(i18nModel, "i18n");
-        },
-
         onShowHello: function(){
          // read msg from i18n model
          var oBundle = this.getView().getModel("i18n").getResourceBundle();
--- webapp/index.js
+++ webapp/index.js
@@ -1,12 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/XMLView"
-], function(XMLView) {
+    "sap/ui/core/ComponentContainer"
+], function(ComponentContainer) {
     'use strict';
 
-    XMLView.create({
-        viewName: "mysap.ui5.walkthrough.view.App"
-    }).then(function (oView) {
-        oView.placeAt("content");
-    });    
+    new ComponentContainer({
+		name: "mysap.ui5.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
     
 });
