9d3c7a20b91fb9588a2b988ee4f04e43e89fdb26
xras-386 <67341079+xras-386@users.noreply.github.com>
2022-07-09 17:56:47 +0200

    git commit -m "Step 16: Dialogs and Fragments"

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
-], function(Controller, MessageToast) {
+    'sap/ui/core/Fragment'
+], function(Controller, MessageToast, Fragment) {
     'use strict';
         return Controller.extend("mysap.ui5.walkthrough.controller.HelloPanel", {
             onShowHello: function(){
@@ -11,6 +12,18 @@ sap.ui.define([
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
             // show message
             MessageToast.show(sMsg);
-            } 
+            },
+            onOpenDialog : function () {
+
+                // create dialog lazily
+                if (!this.pDialog) {
+                    this.pDialog = this.loadFragment({
+                        name: "mysap.ui5.walkthrough.view.HelloDialog"
+                    });
+                } 
+                this.pDialog.then(function(oDialog) {
+                    oDialog.open();
+                });
+            }
         });
     });
