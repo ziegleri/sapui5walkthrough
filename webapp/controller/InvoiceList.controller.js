84c2bc7d186abb580c2c1221467127e6b936b49c
xras-386 <67341079+xras-386@users.noreply.github.com>
2022-07-10 12:24:24 +0200

    git commit -m "Step 22: Custom Formatters"

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
     'mysap/ui5/walkthrough/controller/BaseController.controller',
-    "sap/ui/model/json/JSONModel"
-], function(BaseController, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter"
+], function(BaseController, JSONModel, formatter) {
     'use strict';
     return BaseController.extend("mysap.ui5.walkthrough.controller.InvoiceList", {
+        formatter: formatter,
         onInit : function () {
 			var oViewModel = new JSONModel({
 				currency: "EUR"
