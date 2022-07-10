8ee1cc7b82936ce0a8d8131aa7ffbf0f5ffb9907
xras-386 <67341079+xras-386@users.noreply.github.com>
2022-07-10 12:37:47 +0200

    git commit -m "Step 23: Filtering"

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     'mysap/ui5/walkthrough/controller/BaseController.controller',
     "sap/ui/model/json/JSONModel",
-    "../model/formatter"
-], function(BaseController, JSONModel, formatter) {
+    "../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(BaseController, JSONModel, formatter, Filter, FilterOperator) {
     'use strict';
     return BaseController.extend("mysap.ui5.walkthrough.controller.InvoiceList", {
         formatter: formatter,
@@ -11,7 +13,20 @@ sap.ui.define([
 				currency: "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
-		}
+		},
+        onFilterInvoices: function(oEvent) {
+            // build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.byId("invoiceList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+        }
     });
     
 });
